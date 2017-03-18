faf49f687368e7a93d348b5f9e342a2c1043da08
IT学习者 <2732291467@qq.com>
2017-03-19 00:46:07 +0800

    第一篇 spring boot 一个简单的启动实例

---
+++ spring-boot-demo1/src/test/java/itxxz/Application.java
@@ -0,0 +1,14 @@
+package itxxz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class Application extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+}
---
+++ spring-boot-demo1/src/test/java/itxxz/controller/DemoController.java
@@ -0,0 +1,13 @@
+package itxxz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@RequestMapping("demo")
+	public String demo(){
+		return "IT学习者-螃蟹";
+	}
+}
